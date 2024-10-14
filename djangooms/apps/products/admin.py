5157e85d62cccd2ac4e489af3b62e631ac5d57be
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-14 12:45:45 -0300

    feat: adicionar product no painel de admin

--- djangooms/apps/products/admin.py
+++ djangooms/apps/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from apps.products.models import Product
 # Register your models here.
+
+class ListingProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name', 'price')
+
+
+admin.site.register(Product, ListingProductAdmin)

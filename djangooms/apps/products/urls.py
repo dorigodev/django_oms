c61effe585343ec96dcf5c3ad2b535483f028c73
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-14 12:46:31 -0300

    feat: adicionado funcionabilidades nos produtos

---
+++ djangooms/apps/products/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
+from apps.products.views import index
+from apps.products.views import create_product
+from apps.products.views import update_product
+from apps.products.views import delete_product
+from apps.products.views import search_product
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('create_product', create_product, name='create_product'),
+    path('update_product/<int:product_id>', update_product, name='update_product'),
+    path('delete_product/<int:product_id>', delete_product, name='delete_product'),
+    path('search_product', search_product, name='search_product'),
+]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- djangooms/setup/urls.py
+++ djangooms/setup/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.users.urls')),
+    path('', include('apps.products.urls'))
 ]
 
 if settings.DEBUG:

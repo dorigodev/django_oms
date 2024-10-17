53638fd1a904e382cbbd6bdfb0656eb20d9d7ecd
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-17 18:30:27 -0300

    feat: add new forms for login and register

--- djangooms/apps/users/forms.py
+++ djangooms/apps/users/forms.py
@@ -5,13 +5,15 @@ import json
 from django.contrib.auth.forms import UserCreationForm
 # Pesquisar sobre: from django.contrib.auth import password_validation
 from apps.users.models import BuyerUser
+from apps.users.models import SupplierUser
+from apps.users.models import AdressUser
 from django.contrib.auth import get_user_model
 
 
-class BuyerRegistrationForm(form.ModelForm):
+class SupplierForm(forms.ModelForm):
     class Meta:
-        model = BuyerUser
-        exclude = ['active']
+        model = SupplierUser
+        fields = 'store_name', 'store_email', 'password1', 'password2'
 
     store_name = forms.CharField(
         label='Nome da Loja',
@@ -29,14 +31,6 @@ class BuyerRegistrationForm(form.ModelForm):
                                        'placeholder': 'Digite o e-mail da sua loja'})
     )
 
-    store_CNPJ = forms.CharField(
-        label='CNPJ da loja',
-        required=True,
-        max_length=14,
-        widget=forms.TextInput(attrs={'class': 'form-control',
-                                      'placeholder': 'Digite o CNPJ da sua loja, sem pontos ou traços'})
-    )
-
     password1 = forms.CharField(
         label='Senha para acesso do painel da sua loja',
         required=True,
@@ -51,13 +45,6 @@ class BuyerRegistrationForm(form.ModelForm):
                                           'placeholder': 'Digite sua senha'})
     )
 
-    def clean_store_email(self):
-        store_email = self.cleaned_data.get('store_email', '')
-        exists = StoreUser.objects.filter(email=store_email).exists()
-        if exists:
-            raise forms.ValidationError('Email já cadastrado')
-        return store_email
-
     def clean_password2(self):
         password1 = self.cleaned_data['password1']
         password2 = self.cleaned_data['password2']
@@ -67,16 +54,75 @@ class BuyerRegistrationForm(form.ModelForm):
             else:
                 return password2
 
+    def clean_store_email(self):
+        store_email = self.cleaned_data.get('store_email', '')
+        exists = SupplierUser.objects.filter(email=store_email).exists()
+        if exists:
+            raise forms.ValidationError('Email já cadastrado')
+        return store_email
+
+
+class AdressUserForm(forms.ModelForm):
+    class Meta:
+        model = AdressUser
+        fields = ['store_CEP', 'store_number', 'store_complement']
+
+    store_CEP = forms.CharField(
+        label='CEP da loja',
+        required=True,
+        max_length=10,
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Digite o CEP da sua loja'})
+    )
+
+    store_number = forms.CharField(
+        label='Número da loja',
+        required=True,
+        max_length=15,
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Número da loja'})
+    )
+
+    store_complement = forms.CharField(
+        label='Complemento da loja',
+        required=False,
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Digite o complemento da sua loja, caso tiver'})
+    )
+
+
+class BuyerUserForm(forms.ModelForm):
+    class Meta:
+        model = BuyerUser
+        fields = ['store_CNPJ', 'store_phone',]
+
+    store_CNPJ = forms.CharField(
+        label='CNPJ da loja',
+        required=True,
+        max_length=14,
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Digite o CNPJ da sua loja, sem pontos ou traços'})
+    )
+
+    store_phone = forms.CharField(
+        label='Telefone da loja',
+        required=True,
+        max_length=25,
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Digite o Telefone da sua loja'})
+    )
+
     def clean_store_CNPJ(self):
         cnaes_permitidos = ['8610101', '8630501', '8630502', '8650003', '8630505',
                             '8690903', '4729699', '4773300', '4775002', '4789099']
         store_CNPJ = self.cleaned_data['store_CNPJ']
-        if StoreUser.objects.filter(cnpj=store_CNPJ).exists():
+        if BuyerUser.objects.filter(cnpj=store_CNPJ).exists():
             raise forms.ValidationError('CNPJ já cadastrado')
-        response = requests.get(f'https://brasilapi.com.br/api/cnpj/v1/{store_CNPJ}')
+        response = requests.get(f'https://publica.cnpj.ws/cnpj/{store_CNPJ}')
         if response.status_code == 200:
             data = response.json()
-            if str(data['cnae_fiscal']) not in cnaes_permitidos:
+            if str(data['estabelecimento']['atividade_principal']['id']) not in cnaes_permitidos:
                 raise forms.ValidationError('O Cnae da sua empresa não é permitido para compras ')
         else:
             raise forms.ValidationError('Não foi possível validar o CNPJ. Verifique o número e tente novamente.')
@@ -99,7 +145,3 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput(attrs={'class': 'form-control',
                                           'placeholder': 'Digite sua senha'})
     )
-
-class BuyerRegistrationForm(forms.ModelForm):
-    class Meta:
-        model = BuyerUser

0b853aefc3fa4e8e1aba81030787fe201563dd7c
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-22 10:25:03 -0300

    feat: add form for order models

---
+++ djangooms/apps/orders/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from apps.orders.models import Order
+from apps.orders.models import OrderItem
+from django.forms import modelformset_factory
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('store', 'shipping',)
+
+
+class OrderItemForm(forms.ModelForm):
+    class Meta:
+        model = OrderItem
+        fields = ('item', 'quantity')
+
+
+OrdemItemFormSet = modelformset_factory(
+    OrderItem,
+    form=OrderItemForm,
+    fields=('item', 'quantity'),
+    extra=1
+)

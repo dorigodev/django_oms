2df1625c8e2333593d8438f91345a940dd21b871
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-22 10:25:20 -0300

    feat: add order in admin painel

--- djangooms/apps/orders/admin.py
+++ djangooms/apps/orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.orders.models import Order
+
 
 # Register your models here.
+class ListingOrderAdmin(admin.ModelAdmin):
+    list_display = ('id', "dateOrder", 'completionDate', 'shipping', 'status')
+    list_display_links = ('id',)
+    search_fields = ('id', )
+
+
+admin.site.register(Order, ListingOrderAdmin)

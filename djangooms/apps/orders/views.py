a459762887aae4fcb34cbe34fba1262db3dea395
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-23 10:09:31 -0300

    feat: add functionality for update user

--- djangooms/apps/orders/views.py
+++ djangooms/apps/orders/views.py
@@ -1,8 +1,8 @@
 from pyexpat.errors import messages
-
+import datetime
 from django.shortcuts import render
 from django.shortcuts import redirect
-from apps.orders.forms import OrderForm, OrdemItemFormSet
+from apps.orders.forms import OrderForm, OrdemItemFormSet, OrderUpdateForm
 from apps.orders.models import Order
 from apps.orders.models import OrderItem
 from django.views.generic import ListView, TemplateView
@@ -13,6 +13,8 @@ def index(request):
     order = Order.objects.all()
     orderItem = OrderItem.objects.all()
     return render(request, 'orders/order_list.html', {'order': order, 'orderItem': orderItem})
+
+
 class OrderAddView(TemplateView):
     template_name = 'orders/order.html'
 
@@ -33,5 +35,16 @@ class OrderAddView(TemplateView):
             return redirect('index')
         return self.render_to_response({'orderForm': order_Form, 'orderItemForm': orderItem_Form, })
 
-def finish_order(request):
-    pedidos = OrderItem.objects.all()
+
+def update_order(request, order_id):
+    order = Order.objects.get(pk=order_id)
+    form = OrderUpdateForm(instance=order)
+    if request.method == 'POST':
+        form = OrderUpdateForm(request.POST, instance=order)
+        if form.is_valid():
+            if form.cleaned_data['status'] == 'Finished':
+                order.completionDate = datetime.datetime.now()
+            form.save()
+
+            return redirect('index')
+    return render(request, 'orders/update_order.html', {'form': form})

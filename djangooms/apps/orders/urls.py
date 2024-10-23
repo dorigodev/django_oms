96ef75b5461f000cf7847c2c5f561a6012d59f0d
Murilo Dorigo. <murillodorigo@gmail.com>
2024-10-23 10:09:31 -0300

    feat: add functionality for update user

--- djangooms/apps/orders/urls.py
+++ djangooms/apps/orders/urls.py
@@ -4,10 +4,12 @@ from django.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 from apps.orders.views import OrderAddView, index
+from apps.orders.views import update_order
 
 urlpatterns = [
     path('create_order', OrderAddView.as_view(), name='create_order'),
     path('view_order', index, name='view_order'),
+    path('update_order/<int:order_id>', update_order, name='update_order'),
 ]
 if settings.DEBUG:
     urlpatterns += static(
